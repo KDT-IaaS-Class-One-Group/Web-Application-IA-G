09dd95ebaf7f6049837e73be01a55f83affc7a94
SDH <personarpg7501@gmail.com>
2023-11-03 10:49:47 +0900

    app.js 모듈 구성(1차)

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("테스트 시작");
+const express = require('express'); // 서버 통신 방식 설계
+const app = express();  //express를 app 객체에 담아 사용하려는 목적
+const http = require("http");  //http 모듈을 사용하기 위한 선언
+const fs = require("fs") //fs 모듈을 사용하기 위한 선언(index.html 페이지와 통신하기 위한 규약)
+
+const server = http.createServer(app); //express 방식으로 서버 생성
+const port = 8080; //포트 미리 선언하여 사용
+
+const io = socketIo(server);//소켓이 필요할지 안할지 나중에 구현하면 결정
+
