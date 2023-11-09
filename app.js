6acc1b6202e131b31382a5715f49dde6f050bdc8
동현 <Garry@example.com>
2023-11-09 18:16:33 +0900

    0.4버전으로 가기위한 연구중

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
+// express 모듈을 불러옵니다.
 const express = require('express');
-const fs = require('fs'); // fs 모듈을 불러옵니다.
-const app = express();
-const port = 1004;
 const router = require('./routes')
 
+const bodyParser = require("body-parser");
+
+// fs 모듈을 불러옵니다.
+const fs = require('fs');
+
+// express 애플리케이션을 생성합니다.
+const app = express();
+
+// 서버의 포트를 1004로 설정합니다.
+const port = 1004;
+
+
+// JSON 형식의 요청 데이터를 파싱하기 위해 express의 미들웨어를 사용합니다.
+
+app.use(bo)
+
 app.use(express.json());
+app.use(express.static(__dirname));
 
-
+// '/' 경로에 라우터를 적용합니다.
 app.use("/", router);
 
-//html 페이지 메시지 내역을 표시하기 위한 코드
-// app.get("/initialData", (req, res) => {
-//     fs.readFile('data.json', 'utf8', (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             res.status(500).send("서버 오류");
-//         } else {
-//             const jsonData = JSON.parse(data);
-//             res.json(jsonData);
-//         }
-//     });
-// });
+// 주석 처리된 코드 블록은 '/initialData' 경로에 대한 GET 요청을 처리하는 코드입니다.
+// 이 코드는 'data.json' 파일을 읽어와 JSON 형식으로 응답합니다.
+// 현재는 주석 처리되어 비활성화 상태입니다.
 
-
-
+// 서버를 지정된 포트(1004)로 실행합니다.
 app.listen(port, () => {
     console.log("서버가 열렸습니다.");
 });
--- routes.js
+++ routes.js
@@ -1,40 +1,81 @@
-const express = require('express')
+// express 모듈을 불러옵니다.
+const express = require('express');
+
+// express의 라우터를 생성합니다.
 const router = express.Router();
+
+// fs 모듈을 불러옵니다.
 const fs = require("fs");
 
 
+// ////////////////////////테스트 1 /////////////////////////////////////// //
+// '/data' 경로에 대한 GET 요청을 처리하는 라우트 핸들러입니다.
+router.get('/data', (req, res) => {
+    // 'data.json' 파일을 읽어옵니다.
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("서버 오류");
+      } else {
+        // 읽어온 JSON 데이터를 파싱합니다.
+        const jsonData = JSON.parse(data);
+  
+        // JSON 데이터를 클라이언트로 보냅니다.
+        res.json(jsonData);
+      }
+    });
+  });
+  // ////////////////////////테스트 1 /////////////////////////////////////// // 주석이여
+
+
+// 정적 파일 제공 설정
+
+
+// 루트 경로('/')에 대한 GET 요청을 처리하는 라우트 핸들러입니다.
 router.get("/", (req, res) => {
+  // 현재 스크립트 파일의 디렉토리에서 'index.html' 파일을 응답으로 보냅니다.
   res.sendFile(__dirname + "/index.html");
 });
 
+
+// '/text' 경로에 대한 POST 요청을 처리하는 라우트 핸들러입니다.
 router.post('/text', (req, res) => {
+  // 요청에서 전송된 데이터를 'chatData' 변수에 저장합니다.
   const chatData = req.body;
 
-  // 서버에서 수행할 작업 (필요한 경우)
+  // 서버에서 수행할 작업을 주석으로 처리했으며, 필요한 경우 여기에 작업을 추가할 수 있습니다.
 
+  // 'data.json' 파일을 읽어옵니다.
   fs.readFile('data.json', 'utf8', (err, data) => {
       if (err) {
           console.error(err);
           res.status(500).send("서버 오류");
       } else {
+          // 읽어온 JSON 데이터를 파싱합니다.
           const jsonData = JSON.parse(data);
-          jsonData.mainContent.inputRecords.push(chatData);
-          fs.writeFile('data.json', JSON.stringify(jsonData), 'utf8', (err) => {
-              if (err) {
-                  console.error(err);
-                  res.status(500).send("서버 오류");
-              } else {
-                  res.json({ message: "data.json에서 전송할 message" });
 
-                  
-                  // html message 전송 내용 확인
-                  const jsonData = JSON.parse(data);
-                  // res.json(jsonData);
+          // 'mainContent.inputRecords' 배열에 새로운 'chatData'를 추가합니다.
+          jsonData.mainContent.inputRecords.push(chatData); //이거 html에서 보낸 req를 추가한거임
 
-              }
-          });
+        // 데이터 전송 확인 테스트
+
+        // 수정된 데이터를 'data.json' 파일에 다시 씁니다.
+        //   fs.writeFile('data.json', JSON.stringify(jsonData), 'utf8', (err) => {
+        //       if (err) {
+        //           console.error(err);
+        //           res.status(500).send("서버 오류");
+        //       } else {
+        //           // 테스트 완료 이 부분은 현재 사용되지 않는 듯합니다.
+        //           res.json({ message: "data.json에서 전송할 message" });
+
+        //           // 수정된 데이터를 클라이언트에 응답으로 보낼 수도 있습니다.
+        //           // const jsonData = JSON.parse(data);
+        //           // res.json(jsonData);
+        //       }
+        //   });
       }
   });
 });
 
-module.exports = router
+// 이 라우터 모듈을 외부로 노출합니다.
+module.exports = router;
